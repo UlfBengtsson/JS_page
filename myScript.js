1852e7bdbc04cc2d799dffaa772510f0c7035209
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2020-10-28 09:58:51 +0100

    added a toggle

--- myScript.js
+++ myScript.js
@@ -1,3 +1,4 @@
+"use strict";
 console.log("from console.log");
 //alert("don´t you unless you must");
 //var name = "<h1>Ulf</h1>";
@@ -11,12 +12,28 @@ document.getElementById("changeMe").classList.add("maxW");
 
 //document.write("this is write");
 
+var boxToggle = true;
+
 function createDivInsideDemo(addClassCss) {
   var myBlock = document.createElement("div");
   myBlock.classList.add(addClassCss);
 
   myDemoElement.appendChild(myBlock);
+
+  if (addClassCss === "blueBlock") {
+    boxToggle = false;
+  } else {
+    boxToggle = true;
+  }
 }
 
 createDivInsideDemo("blueBlock");
 createDivInsideDemo("redBlock");
+
+let a = "bad way to make a varible";
+console.log("var a:", a);
+
+document.getElementById("toggleBox").addEventListener("wheel", (event) => {
+  event.preventDefault();
+  createDivInsideDemo(boxToggle ? "blueBlock" : "redBlock");
+});
