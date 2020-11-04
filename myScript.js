df493224343537d64f33dc43eb91b140be5f8e62
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2020-11-04 09:57:12 +0100

    ex map

--- myScript.js
+++ myScript.js
@@ -1,18 +1,10 @@
-"use strict";
-console.log("from console.log");
-//alert("don´t you unless you must");
-//var name = "<h1>Ulf</h1>";
-
-var myDemoElement = document.getElementById("demo");
-
-//myDemoElement.innerHTML = name;
-//myDemoElement.style.color = "blue";
-
-document.getElementById("changeMe").classList.add("maxW");
-
-//document.write("this is write");
-
+"use strict"; //https://www.w3schools.com/js/js_strict.asp
 var boxToggle = true;
+var myDemoElement = document.getElementById("demo");
+document.getElementById("changeMe").classList.add("maxW");
+console.log("from console.log");
+
+//------------------- functions section --------------------------------------------------------------------
 
 function createDivInsideDemo(addClassCss) {
   var myBlock = document.createElement("div");
@@ -27,13 +19,35 @@ function createDivInsideDemo(addClassCss) {
   }
 }
 
-createDivInsideDemo("blueBlock");
-createDivInsideDemo("redBlock");
-
-let a = "bad way to make a varible";
-console.log("var a:", a);
+//-------------------- Event Listeners ----------------------------------------------------------------------
 
 document.getElementById("toggleBox").addEventListener("wheel", (event) => {
   event.preventDefault();
+  console.log(event);
   createDivInsideDemo(boxToggle ? "blueBlock" : "redBlock");
 });
+
+//-------------------- workshop --------------------------------------------
+
+var ticTacToeArray = [
+  ["blueBlock", "redBlock", "blueBlock"],
+  ["redBlock", "blueBlock", "redBlock"],
+  ["blueBlock", "redBlock", "blueBlock"],
+];
+
+function createMap(mapArray) {
+  console.log("map length", mapArray[0].length);
+  myDemoElement.style.height = 50 * mapArray.length + "px";
+  myDemoElement.style.width = 50 * mapArray[0].length + "px";
+
+  for (let y = 0; y < mapArray.length; y++) {
+    for (let x = 0; x < mapArray[y].length; x++) {
+      let myBlock = document.createElement("div");
+      myBlock.classList.add(mapArray[y][x]);
+      myBlock.id = "y" + y + "x" + x;
+      myDemoElement.appendChild(myBlock);
+    }
+  }
+}
+
+createMap(ticTacToeArray);
